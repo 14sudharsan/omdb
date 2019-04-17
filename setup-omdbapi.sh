76719208899856a2286d167f7484e695fea8535b
14sudharsan <14sudharsan@gmail.com>
2019-04-17 20:08:41 +0530

    Update setup-omdbapi.sh

--- setup-omdbapi.sh
+++ setup-omdbapi.sh
@@ -2,4 +2,4 @@
 echo Downloading omdbapi docker image please wait ...
 docker pull 14sudharsan/omdb:version1
 echo Docker container spawn from container image
-docker run -it 14sudharsan/omdb:version1 bash
+docker run -it 14sudharsan/omdb:version1 
