66e35106c2df28a41cca4c3f07fa2a8f2bda82de
14sudharsan <14sudharsan@gmail.com>
2019-04-17 17:50:25 +0530

    Delete mymovie.sh

--- mymovie.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#echo Movie Name $1
-read -p " Enter the MovieName : " moviename
-echo $moviename
-curl -Gs "http://www.omdbapi.com/?t=$moviename&apikey=13f411c2" | jq ".Ratings[1]"
